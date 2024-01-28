fe1f7ce177a4f1b7ea21e62bafb24fff7b2eebd0
Jean Flaherty <kobejean@me.com>
2024-01-28 11:58:48 +0900

    update

--- rfqa/method_configs.py
+++ rfqa/method_configs.py
@@ -11,6 +11,7 @@ from typing import Dict, Union
 import tyro
 
 from nerfstudio.data.datamanagers.parallel_datamanager import ParallelDataManagerConfig
+from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManager, VanillaDataManagerConfig
 from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
 from nerfstudio.configs.base_config import ViewerConfig
 from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataParserConfig
@@ -21,8 +22,11 @@ from nerfstudio.engine.schedulers import (
     MultiStepSchedulerConfig,
 )
 from nerfstudio.engine.trainer import TrainerConfig
-from nerfstudio.models.nerfacto import NerfactoModelConfig
+from rfqa.nerfacto_model import NerfactoModelConfig
+from rfqa.instant_ngp_model import InstantNGPModelConfig
 from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
+from nerfstudio.pipelines.dynamic_batch import DynamicBatchPipelineConfig
+
 
 from nerfstudio.plugins.types import MethodSpecification
 from rfqa.blender_dataparser import BlenderDataParserConfig
@@ -120,3 +124,32 @@ rfqa_nerfacto_big = MethodSpecification(
     description="Nerfstudio method template big.",
 )
 
+
+
+
+rfqa_instant_ngp = MethodSpecification(
+    config=TrainerConfig(
+        method_name="instant-ngp",
+        steps_per_eval_batch=500,
+        steps_per_save=2000,
+        max_num_iterations=30000,
+        mixed_precision=True,
+        pipeline=DynamicBatchPipelineConfig(
+            datamanager=VanillaDataManagerConfig(
+                dataparser=NerfstudioDataParserConfig(),
+                train_num_rays_per_batch=4096,
+                eval_num_rays_per_batch=4096,
+            ),
+            model=InstantNGPModelConfig(eval_num_rays_per_chunk=8192),
+        ),
+        optimizers={
+            "fields": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+            }
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=1 << 12),
+        vis="viewer",
+    ),
+    description="Nerfstudio method template instant-ngp.",
+)
